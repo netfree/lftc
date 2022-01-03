d1bc47a9cd7432784bd6381b4faac82ddb064adc
Mihalache Ștefan Cristian <57230349+StefanCsPurge@users.noreply.github.com>
2022-01-03 20:32:10 +0200

    Update main.py

--- parser_andrei-muntean_doris-moisuc/lab-echipa/main.py
+++ parser_andrei-muntean_doris-moisuc/lab-echipa/main.py
@@ -4,3 +4,5 @@ grammar = GrammarFileParser.parse("g1.txt")
 grammar.print_terminals()
 grammar.print_non_terminals()
 grammar.print_productions()
+
+# maybe test the other implemented stuff here - e.g. sequence parsing
